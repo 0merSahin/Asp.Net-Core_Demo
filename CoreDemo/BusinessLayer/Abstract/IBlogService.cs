d40155ad83645f451d2ebd4892c9152112b56a54
Ömer Şahin <omersahin0309@gmail.com>
2023-09-17 18:06:15 +0300

    Updated

---
+++ CoreDemo/BusinessLayer/Abstract/IBlogService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Abstract
+{
+	public interface IBlogService
+	{
+        void CategoryAdd(Blog blog);
+
+        void CategoryDelete(Blog blog);
+
+        void CategoryUpdate(Blog blog);
+
+        List<Blog> GetList();
+
+        Blog GetById(int id);
+    }
+}
+
---
+++ CoreDemo/BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class BlogManager : IBlogService
+    {
+        IBlogDal _blogDal;
+
+        public BlogManager(IBlogDal blogDal)
+        {
+            _blogDal = blogDal;
+        }
+
+        public void CategoryAdd(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CategoryDelete(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CategoryUpdate(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Blog GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetList()
+        {
+            return _blogDal.GetListAll();
+        }
+    }
+}
+
