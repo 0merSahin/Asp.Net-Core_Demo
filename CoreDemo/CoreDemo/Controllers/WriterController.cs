0641a8937d6f321e399c97b13b5c132498149491
Ömer Şahin <omersahin0309@gmail.com>
2023-11-20 14:09:28 +0300

    Updated

---
+++ CoreDemo/CoreDemo/Controllers/WriterController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    // [Authorize] --> bu kodu buraya yazarsak, tüm controller Authorize edilmiş olur.
+
+    public class WriterController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+
+        public IActionResult WriterMail()
+        {
+            return View();
+        }
+    }
+}
+
